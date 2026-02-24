f00ac39b1c376974479337836e0387ab1eab9cbf
Evandro Ferraz <ferraz.evandro@outlook.com>
2026-02-24 14:38:06 -0300

    aula 2402 - programando redimensionamento + novos testes

--- semanas02&03 - vetor dinâmico/NossoVetor.java
+++ semanas02&03 - vetor dinâmico/NossoVetor.java
@@ -1,8 +1,8 @@
-// herda da classe Object
+// Herda método toString() da classe Object
 public class NossoVetor {
     private int vetor[];  // vetor para armazenar os elementos na memória
     private int ocupacao = 0; // quantidade de elementos efetivamente inseridos no vetor
-    private int capacidade = 0;
+    private int capacidade = 0; // capacidade atual do vetor
 
     // Construtor 1: inicializa o vetor com capacidade para 10 elementos
     public NossoVetor () {
@@ -22,13 +22,13 @@ public class NossoVetor {
         for(int j=0; j < ocupacao; j++){
             temp[j] = vetor[j];
         }
-        vetor = temp; //temp sendo a cópia de vetor porém com a capacidade aumentada
+        vetor = temp; //temp é uma cópia de vetor porém com a capacidade aumentada
         capacidade = novaCapacidade; // atualiza o atributo capacidade
     }
 
     // Retorna o tamanho do vetor (sua capacidade)
     public int getCapacidade () {
-        return vetor.length;
+        return capacidade;
     }
     
     // Verifica se existe pelo menos um elemento no vetor
@@ -38,29 +38,26 @@ public class NossoVetor {
     
     // Verifica se a ocupacao (número de elementos inseridos) alcançou a capacidade do vetor
     public boolean estaCheio () {
-        return ocupacao == vetor.length;
+        return ocupacao == capacidade;
     }
     
     // Adiciona um elemento na primeira posição vazia (vetor[ocupacao])
     public void adiciona (int elemento) {
-        if (estaCheio()) redimensiona(capacidade*2);
-        // Impede a inserção se o vetor estiver cheio
+        if (estaCheio()) redimensiona(capacidade*2); // Aumenta a capacidade do vetor se estiver cheio
         // ocupacao++ atua como pós-incremento: 
         // 1º acessa o índice atual, 2º insere o elemento, 3º incrementa a variável ocupação computando que mais um número foi inserido no vetor
         vetor[ocupacao++] = elemento;
-        // inserindo o número 5 no vetor |2|4|0|0|, ficaria |2|4|5|0|
     }
 
     // Remove o último elemento do vetor, reduz a capacidade se necessário e retorna o elemento removido
     public int remove(){
         if(estaVazio()) return -1;
         int removido = vetor[--ocupacao];
-        // remover um número do vetor |2|4|5|0|, removeria o numero 5
         if(capacidade >= 10 && ocupacao <= capacidade/4) redimensiona(capacidade/2);
         return removido;
     }
     
-    // Realiza uma busca linear. 
+    // Realiza uma busca linear.
     // Devolve o índice da primeira ocorrência do elemento, ou -1 caso não seja encontrado.
     public int procuraIndice (int elemento) {
         for (int i = 0; i < ocupacao; i++) {
@@ -69,7 +66,7 @@ public class NossoVetor {
         return -1; // Elemento não existe no vetor
     }
 
-    // método que retorna um valor booleano caso existe o elemento no vetor
+    // Retorna um valor booleano caso o elemento exista no vetor
     public boolean contains(int elemento){
         for(int i=0; i < ocupacao; i++){
             if(vetor[i] == elemento) return true;
@@ -77,9 +74,8 @@ public class NossoVetor {
         return false;
     }
 
-    // método que lista as posições ocupadas por um elemento especifico no vetor
+    // Lista todas as posições ocupadas por um determinado elemento
     public NossoVetor listaPosicoes(int elemento){
-        // |2|4|5|2|, elemento = 2, ocupacao = 4
         NossoVetor lista = new NossoVetor(this.ocupacao);
         for(int i=0; i < this.ocupacao; i++){
             if(this.vetor[i] == elemento){
@@ -95,7 +91,7 @@ public class NossoVetor {
     @Override
     public String toString () {
         String s = "[ ";
-        // Percorre apenas até a "ocupacao" (número de elementos inseridos), ignorando as posições vazias do vetor
+        // Percorre apenas até a "ocupacao" (número de elementos inseridos), ignorando as posições vazias
         for (int i = 0; i < ocupacao; i++) {
             s += vetor[i] + " ";
         }
