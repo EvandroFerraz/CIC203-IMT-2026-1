7f8f2d3b96bd8564a95581e1c777682bdf399502
EVANDRO CATELANI FERRAZ <evandro.ferraz@maua.br>
2026-02-25 12:54:08 -0300

    aula2502 - programação do redimensionamento no vetor dinâmico

--- t1-sub1-lab/semana02 - vetor dinâmico - parte 1/NossoVetor.java
+++ t1-sub1-lab/semana02 - vetor dinâmico - parte 1/NossoVetor.java
@@ -1,20 +1,34 @@
+// Herda método toString() da classe Object
 public class NossoVetor {
     private int vetor[];  // vetor para armazenar os elementos na memória
     private int ocupacao = 0; // quantidade de elementos efetivamente inseridos no vetor
+    private int capacidade = 0; // capacidade atual do vetor
 
     // Construtor 1: inicializa o vetor com capacidade para 10 elementos
     public NossoVetor () {
         vetor = new int[10];
+        capacidade = 10;
     }
     
     // Construtor 2: permite definir a capacidade inicial do vetor
     public NossoVetor (int capacidadeInicial) {
         vetor = new int[capacidadeInicial];
+        capacidade = capacidadeInicial;
     }
     
+    // Redimensiona a capacidade do vetor
+    private void redimensiona(int novaCapacidade){
+        int[] temp = new int[novaCapacidade];
+        for(int j=0; j < ocupacao; j++){
+            temp[j] = vetor[j];
+        }
+        vetor = temp; //temp é uma cópia de vetor porém com a capacidade aumentada
+        capacidade = novaCapacidade; // atualiza o atributo capacidade
+    }
+
     // Retorna o tamanho do vetor (sua capacidade)
     public int getCapacidade () {
-        return vetor.length;
+        return capacidade;
     }
     
     // Verifica se existe pelo menos um elemento no vetor
@@ -24,19 +38,26 @@ public class NossoVetor {
     
     // Verifica se a ocupacao (número de elementos inseridos) alcançou a capacidade do vetor
     public boolean estaCheio () {
-        return ocupacao == vetor.length;
+        return ocupacao == capacidade;
     }
     
     // Adiciona um elemento na primeira posição vazia (vetor[ocupacao])
-    public boolean adiciona (int elemento) {
-        if (estaCheio()) return false; // Impede a inserção se o vetor estiver cheio
+    public void adiciona (int elemento) {
+        if (estaCheio()) redimensiona(capacidade*2); // Aumenta a capacidade do vetor se estiver cheio
         // ocupacao++ atua como pós-incremento: 
         // 1º acessa o índice atual, 2º insere o elemento, 3º incrementa a variável ocupação computando que mais um número foi inserido no vetor
-        vetor[ocupacao++] = elemento; 
-        return true;
+        vetor[ocupacao++] = elemento;
+    }
+
+    // Remove o último elemento do vetor, reduz a capacidade se necessário e retorna o elemento removido
+    public int remove(){
+        if(estaVazio()) return -1;
+        int removido = vetor[--ocupacao];
+        if(capacidade >= 10 && ocupacao <= capacidade/4) redimensiona(capacidade/2);
+        return removido;
     }
     
-    // Realiza uma busca linear. 
+    // Realiza uma busca linear.
     // Devolve o índice da primeira ocorrência do elemento, ou -1 caso não seja encontrado.
     public int procuraIndice (int elemento) {
         for (int i = 0; i < ocupacao; i++) {
@@ -44,17 +65,36 @@ public class NossoVetor {
         }
         return -1; // Elemento não existe no vetor
     }
+
+    // Retorna um valor booleano caso o elemento exista no vetor
+    public boolean contains(int elemento){
+        for(int i=0; i < ocupacao; i++){
+            if(vetor[i] == elemento) return true;
+        }
+        return false;
+    }
+
+    // Lista todas as posições ocupadas por um determinado elemento
+    public NossoVetor listaPosicoes(int elemento){
+        NossoVetor lista = new NossoVetor(this.ocupacao);
+        for(int i=0; i < this.ocupacao; i++){
+            if(this.vetor[i] == elemento){
+                lista.adiciona(i);
+            }
+        }
+        return lista;
+    }
+
     
     // Sobrescrita do método toString() herdado da classe Object. 
     // Permite que o print exiba os dados reais do vetor em vez do seu endereço de memória.
     @Override
     public String toString () {
         String s = "[ ";
-        // Percorre apenas até a "ocupacao" (número de elementos inseridos), ignorando as posições vazias do vetor
+        // Percorre apenas até a "ocupacao" (número de elementos inseridos), ignorando as posições vazias
         for (int i = 0; i < ocupacao; i++) {
             s += vetor[i] + " ";
         }
         return s + "]";
     }
-
 }
---
+++ t1-sub1-lab/semana02 - vetor dinâmico - parte 1/OutroTeste.java
@@ -0,0 +1,36 @@
+public class OutroTeste{
+    public static void main(String[] args){
+        
+        NossoVetor v = new NossoVetor(); // capacidade = 10
+        
+        // adiciona 5 numeros
+        v.adiciona(2);
+        v.adiciona(10);
+        v.adiciona(2);
+        v.adiciona(5);
+        v.adiciona(8); // ocupacao = 5, capacidade = 10
+
+        System.out.println(v.listaPosicoes(2)); // retorna [0 2]
+
+        // adiciona +5 numeros
+        v.adiciona(20);
+        v.adiciona(11);
+        v.adiciona(23);
+        v.adiciona(54);
+        v.adiciona(81); // ocupacao = 10, capacidade = 10
+        
+        System.out.println(v.getCapacidade()); // retorna 10
+        
+        // extrapola a capacidade atual de v e o método redimensiona é chamado
+        v.adiciona(99); // ocupacao = 11, capacidade = 20
+
+        System.out.println(v.getCapacidade()); // retorna 20
+
+        // remove 8 elementos do vetor
+        for(int i = 0; i < 8; i++){
+            v.remove();
+        } // ocupacao = 3, capacidade = 10
+
+        System.out.println(v.getCapacidade()); // retorna 10
+    }
+}
