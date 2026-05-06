0286f6d0a5ce9d7b93c15aaffd896f06ae705635
evandro <ferraz.evandro@outlook.com>
2026-05-06 12:34:33 -0300

    06-05: arvoreBusca - implementando contagem de altura e nos

--- t1-sub1-lab/semana10 - arvore binaria de busca - conceito, insercao e percorrer/ArvoreBinariaBusca.java
+++ t1-sub1-lab/semana10 - arvore binaria de busca - conceito, insercao e percorrer/ArvoreBinariaBusca.java
@@ -91,4 +91,74 @@ public class ArvoreBinariaBusca{
             s += stringEmOrdemRec(atual.getDireita());
         return s;
     }
+
+    // 1- calcular a altura da árvore
+    // Altura: o número de níveis no caminho mais longo da raiz até uma folha
+    public int altura(){
+        // Se a árvore está vazia ou se existe um único nó, altura = 0
+        if(arvoreVazia() || (raiz.getEsquerda() == null && raiz.getDireita() == null))
+            return 0;
+
+        // Caso contrário, chama o método recursivo para calcular a altura a partir da raiz
+        return alturaRec(raiz);
+    }
+    private int alturaRec(No atual){
+        // Caso Base (critério de parada): Se o nó atual é uma folha, retorna 0
+        if(atual.getEsquerda() == null && atual.getDireita() == null)
+            return 0;
+        
+        // Incializa variáveis para armazenar as alturas da subárvore esquerda e direita
+        int alturaEsquerda = 0, alturaDireita = 0;
+
+        // Se existe uma subárvore à esquerda, chamamos o método recursivo para ela
+        if(atual.getEsquerda() != null)
+            alturaEsquerda = alturaRec(atual.getEsquerda());
+
+        // Se existe uma subárvore à direita, chamamos o método recursivo para ela
+        if(atual.getDireita() != null)
+            alturaDireita = alturaRec(atual.getDireita());
+
+        // Retorna a maior altura entre os filhos esquerdo e direito, incrementada
+        // em +1 (nível do nó atual)
+        return alturaDireita > alturaEsquerda ? alturaDireita + 1 : alturaEsquerda + 1;
+    }
+    // 2- contar quantos nós a árvore tem
+    /* Teste de Mesa
+            8
+           / 
+          5
+        /  \ 
+       2     7 
+       1ª chamada recursiva: contaNosRec(8), gera a 2ª chamada
+       2ª chamada recursiva: contaNosRec(5), gera a 3ª chamada e 4ª chamada
+       3ª chamada recursiva: contaNosRec(2), não tem filhos então retorna 1.
+       4ª chamada recursiva: contaNosRec(7), não tem filhos então retorna 1.
+       Com o resultado da 3ª e 4ª chamada, voltamos resolvendo a pilha de recursão
+       - resolve a 2ª chamada: contaNosRec(5), recebe 1 como retorno da 3ª e 4ª chamada
+       e incrementa esses valores na variável "nos", retornando o valor incrementado
+       no retorno. Então, retorna 3.
+       - resolve a 1ª chamada: contaNosRec(8), recebe o 3 como retorno da 2ª chamada
+       e incrementa esse valor na variável "nos", retornando o valor incrementado no
+       retorno. Então, retorna 4.
+    */
+
+    public int contaNos(){  
+        if(arvoreVazia()) return 0;
+        return contaNosRec(raiz); // primeira chamada = raiz da árvore
+    }
+    public int contaNosRec(No atual){
+         int nos = 0; // guarda o número de nós atual
+
+         // Percorremos primeiro as subárvores esquerda e depois as direitas
+         if(atual.getEsquerda() != null){
+            nos += contaNosRec(atual.getEsquerda());
+         }
+         if(atual.getDireita() != null){
+            nos += contaNosRec(atual.getDireita());
+         }
+         return nos + 1;
+    }
+
+    // 3- remover um nó da árvore
+    
 }
--- t1-sub1-lab/semana10 - arvore binaria de busca - conceito, insercao e percorrer/TesteABB.java
+++ t1-sub1-lab/semana10 - arvore binaria de busca - conceito, insercao e percorrer/TesteABB.java
@@ -1,15 +1,15 @@
 import java.util.Random;
 
-public class TesteABB{
-    
-    public static void main(String args[]){
-        
+public class TesteABB {
+
+    public static void main(String args[]) {
+
         Random random = new Random();
         ArvoreBinariaBusca abb = new ArvoreBinariaBusca(); // raiz = null, nasce vazia
         ArvoreBinariaBusca abbZip = new ArvoreBinariaBusca(); // raiz = null, nasce vazia
 
         // Preenche a árvore
-        for(int i = 1; i <= 100; i++){
+        for (int i = 1; i <= 100; i++) {
             int n = random.nextInt(100); // entre 0 e 99
             abb.insere(n);
             abbZip.insereRepeticao(n);
@@ -17,5 +17,11 @@ public class TesteABB{
 
         System.out.println("Arvore Original: \n" + abb);
         System.out.println("Arvore Compactada: \n" + abbZip);
+
+        System.out.println("Altura da Original: \n" + abb.altura());
+        System.out.println("Altura da Compactada: \n" + abbZip.altura());
+
+        System.out.println("Nós da Original: \n" + abb.contaNos());
+        System.out.println("Nós da Compactada: \n" + abbZip.contaNos());
     }
 }
